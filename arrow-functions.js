7b97184f7fa45a8c16e029fa28d37c03527bba65
Zoe Heathcote <zoe08@hotmail.co.uk>
2024-06-11 19:02:43 +0000

    tested arrow functions code

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,39 @@
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
+// // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3 ,5)
+// console.log(sum);
 
-// Arrow Function With Parameters
+// // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+//     return a+ b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum)
 
 // Single Line Arrow Function With Parameters
+// Or still valid: const addTwoNumbers2 = (a, b) => (a+ b);
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!');
+// or something like,
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
